b1ace03654756447fb0437f373894ea64637ac0a
Rdxer <240302832@qq.com>
2015-12-31 11:14:38 +0800

    init commit

---
+++ LXF-Categories/LXF-Categories/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXF-Categories
+//
+//  Created by LXF on 15/12/31.
+//  Copyright © 2015年 LXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXF-Categories/LXF-Categories/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LXF-Categories
+//
+//  Created by LXF on 15/12/31.
+//  Copyright © 2015年 LXF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
